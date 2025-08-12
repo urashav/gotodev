b529b94530b6af40cf9eeba742a334359021a3b8
Artyom Yurash <urashav@ya.ru>
2025-08-12 13:30:39 +0300

    mid/chan-completion.go

--- concurrency/mid/chan-completion.go
+++ concurrency/mid/chan-completion.go
@@ -10,17 +10,21 @@ import (
 // Канал завершения
 // Есть функция, которая произносит текст пословно (с некоторыми задержками):
 
-func say(id int, text string) {
+func say(done chan struct{}, id int, text string) {
 	for _, word := range strings.Fields(text) {
 		fmt.Printf("Worker #%d says: %s...\n", id, word)
 		dur := time.Duration(rand.Intn(100)) * time.Millisecond
 		time.Sleep(dur)
 	}
+	done <- struct{}{}
 }
 
 // Запускаем несколько одновременных воркеров, по одной на каждую фразу:
 
 func main() {
+	done := make(chan struct{})
+	defer close(done)
+
 	phrases := []string{
 		"go is awesome",
 		"cats are cute",
@@ -29,7 +33,11 @@ func main() {
 		"floor is lava",
 	}
 	for idx, phrase := range phrases {
-		go say(idx+1, phrase)
+		go say(done, idx+1, phrase)
+	}
+
+	for _ = range len(phrases) {
+		<-done
 	}
 }
 
